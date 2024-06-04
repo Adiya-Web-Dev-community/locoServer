dab9dd9e4654d8c7eed3374c2087984031bc9cba
Mohammad Shahzad <shahzad201415@gmail.com>
2024-06-04 11:12:02 +0530

    chnages

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 mongoose.set("strictQuery", false);
 require("dotenv").config();
-mongoose.connect(process.env.MONGO_URL).then(() => {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
     console.log("Database connected");
-  }).catch((err) => {
+  })
+  .catch((err) => {
     console.log("connection failed", err);
   });
-  app.use(require("./route/userRoute.js"))
-  app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-  });
+app.use(require("./route/userRoute.js"));
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
